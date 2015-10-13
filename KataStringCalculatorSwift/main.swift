6b56e8269e0987aafce563ccd7e825ea6a6d57f0
pedrovgs <pedrovicente.gomez@gmail.com>
2015-10-14 01:21:55 +0200

    Create an empty command line project in swift

---
+++ KataStringCalculatorSwift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  KataStringCalculatorSwift
+//
+//  Created by Pedro Vicente on 14/10/15.
+//  Copyright © 2015 Pedro Vicente Gómez Sánchez. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
