5726966f33956f97d8888730877382caa47a19ea
pedrovgs <pedrovicente.gomez@gmail.com>
2015-10-17 11:32:16 +0200

    Refactoring. Extract String and Int extensions to a extensions.swift file

--- KataStringCalculatorSwift/NumberExtractor.swift
+++ KataStringCalculatorSwift/NumberExtractor.swift
@@ -44,35 +44,3 @@ class NumberExtractor {
     }
     
 }
-
-extension String {
-    
-    subscript(integerIndex: Int) -> Character {
-        let index = self.startIndex.advancedBy(integerIndex)
-        return self[index]
-    }
-    
-    subscript(integerRange: Range<Int>) -> String {
-        let start = self.startIndex.advancedBy(integerRange.startIndex)
-        let end = self.startIndex.advancedBy(min(self.characters.count, integerRange.endIndex))
-        let range = start..<end
-        return self[range]
-    }
-    
-    func countChars() -> Int {
-        return characters.count
-    }
-}
-
-extension Int {
-    
-    func countDigits() -> Int {
-        var n = self
-        var sum = 0
-        while n > 0 {
-            sum += n % 10
-            n /= 10
-        }
-        return sum
-    }
-}
